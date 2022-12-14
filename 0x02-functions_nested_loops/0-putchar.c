01f4765fb91d1879a4d0ebc4631955e87c1acb6e
kaynets <emeka.opara4@gmail.com>
2022-12-14 06:54:12 -0800

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putcharh and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	-putchar('r');
+	_putchar('\n');
+
+	return  (0);
+}
